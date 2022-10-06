5749df408619489149a2cca6e9ced2906b327aed
BETA <nabilhassan223@gmail.com>
2022-10-06 07:42:54 -0700

    final changes

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import os
+import datetime
 
-os.system("Color 0a")
+os.system("Color 0a & ascii-art AutoFastQC.ico --height 50 --width 100")
 
 print(">>Welcome to IK Cohort Auto QC.\n",
       "This software should be used to check cohorts automatically\n",
       "Designed and developed: Nabil Hassan\n\n",
-      "_"*80
+      "_" * 80
       )
 
 try:
@@ -15,16 +16,14 @@ try:
 except:
     input("There was a problem reading the file")
 
-try:
-    primary = pd.read_csv(prim, skiprows=3,
-                          parse_dates=[["Start Date", 'Start Time'], ['End Date', 'End Time']], keep_default_na='')
-    secondary = pd.read_csv(sec, sep='|', skiprows=1,
-                            parse_dates=[["Start Date", 'Start Time'], ['End Date', 'End Time']], keep_default_na='')
-except:
-    input("There was a problem processing the files.")
-    quit()
+# try:
+primary = pd.read_csv(prim, skiprows=3, parse_dates=[["Start Date", 'Start Time'], ['End Date', 'End Time']], keep_default_na='')
+secondary = pd.read_csv(sec, sep='|', skiprows=1, parse_dates=[["Start Date", 'Start Time'], ['End Date', 'End Time']], keep_default_na='')
+# except:
+#     input("There was a problem processing the files.")
+#     quit()
 
-if primary.shape == secondary.shape:
+if primary.shape[0] == secondary.shape[0]:
     print(f"{primary.shape} is the shape of both datas")
 else:
     print(f"Data shape of \n Primary = {primary.shape} \n Secondary = {secondary.shape}")
@@ -38,6 +37,7 @@ else:
 
 headers = list(primary.keys())
 headers.remove('Topic Code')
+headers.remove('Status')
 final_data = {'Origin': [], 'No of error': []}
 error = 0
 error_freq_list = []
@@ -45,34 +45,36 @@ error_freq_list = []
 for header in headers:
     final_data[header] = []
 
-try:
-    for i in range(len(primary)):
-        error_in_this_data_row = 0
-        for header in headers:
-            final_data[header].append(primary[header][i])
-            if (primary[header][i]) != (secondary[header][i]):
-                final_data[header].append(secondary[header][i])
-                error += 1
-                error_in_this_data_row += 1
-            else:
-                final_data[header].append('')
+# try:
+for i in range(len(primary)):
+    error_in_this_data_row = 0
+    for header in headers:
+        final_data[header].append(primary[header][i])
+        if (primary[header][i]) != (secondary[header][i]):
+            final_data[header].append(str(secondary[header][i]) + "(Missing/Wrong value)")
+            error += 1
+            error_in_this_data_row += 1
+        else:
             final_data[header].append('')
-        for item in ['Source', 'Uplevel', '']:
-            final_data['Origin'].append(item)
-        for j in range(2):
-            final_data['No of error'].append(f'Errors : {error_in_this_data_row}')
-        final_data['No of error'].append('')
-        error_freq_list.append(error_in_this_data_row)
-except:
-    input("There was a problem in generating the score")
-    quit()
+        final_data[header].append('')
+    for item in ['Source', 'Uplevel', '']:
+        final_data['Origin'].append(item)
+    for j in range(2):
+        final_data['No of error'].append(f'Errors : {error_in_this_data_row}')
+    final_data['No of error'].append('')
+    error_freq_list.append(error_in_this_data_row)
+# except:
+#     input("There was a problem in generating the score")
+#     quit()
 
 # print('Error frequency list', error_freq_list)
 # print("Total No of Errors = -", error)
-print("Score =", error)
+print("Score = -", error)
+
 
 final_data = pd.DataFrame(final_data)
-final_data.to_csv("final_data.csv")
-print("Output file generated final_data.csv")
-input("Press enter to exit.")
+file_name = f"Output({str(datetime.datetime.now().strftime('%d_%m_%y %H_%M_%S'))}).csv"
+final_data.to_csv(file_name)
+print(f"Output file generated {file_name}")
 
+input("Press enter to exit.")
